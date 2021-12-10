302245e0b778189b8cd88ba2528dc2d77f4cbe36
cham1985 <397866153@qq.com>
2021-12-10 15:09:23 +0800

    add code
    Signed-off-by: cham1985 <397866153@qq.com>

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ import arrayTools from './tools/arrayTools'
 import * as Math from './tools/Math'
 import objTools from './tools/objTools'
 import tool from './tools/tool'
-// @ts-ignore
-// import rootLess from './style/root.less' Error: Unexpected token (Note that you need plugins to import files that are not JavaScript)
 
 export {
   dateTools, stringTools,ahooks,arrayTools,Math,objTools,tool
