469269d56b4db2666a4996f7d6de5205599e818b
Stark8964911 <397866153@qq.com>
2022-04-02 17:07:01 +0800

    add queryPageParam

--- README.md
+++ README.md
@@ -15,15 +15,16 @@ Usage:
 
         import {  } from 'starkfrontendtools';
 
-Push:
-
-        1 change the version of your package.json every time before npm publish
-        2 git commit -a -s -m ''
-        3 git push origin master
-
 Publish:
 
         npm login
         npm publish 
             note: change the version of your package.json every time before npm publish
+
+Push:
+
+        git commit -a -s -m ''
+        git push origin master
+
+
         
--- src/tools/vueTools.ts
+++ src/tools/vueTools.ts
@@ -1,10 +1,25 @@
 /**
- * 获取vue的路由页面参数
- * Get route page parameters of Vue
+ * 获取vue的路由页面参数 | Get route page parameters of Vue
  * @param $route
+ * @param queryKey : 要找的参数的key | Key of the parameter to be found
  */
-const queryPageParam= ($route)=> {
+const queryPageParam= ($route:{query: {}},queryKey:string='')=> {
     console.log('vueTools.ts queryPageParam $route=,', $route)
+    console.log('vueTools.ts queryPageParam queryKey=,', queryKey)
+    let res=null
+
+    const {query}=$route
+    if (query) {
+        // @ts-ignore
+        res=query[queryKey]
+    }
+    if (!res) {
+        const urlParams = new URLSearchParams(window.location.search)
+        console.log('vueTools.ts queryPageParam urlParams=', urlParams.toString())
+        res = urlParams.get(queryKey)
+    }
+    console.log('vueTools.ts queryPageParam res=,', res)
+    return res
 }
 
 export default {
